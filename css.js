a7001d3ed88cca3c764fae192d84b7eb31fc7bb2
Xana Green <byuixana@byui.edu>
2022-09-23 08:27:33 -0700

    adding friday code

---
+++ css.js
@@ -0,0 +1,7 @@
+function changeText() {
+    let newtext = 'Hi there!';
+    let oldtext = 'This is some sweet text';
+    document.getElementById('divText').innerHTML;
+    document.getElementById('divText').innerHTML;
+    let pageText = document.getElementById('divWithText')
+} 
